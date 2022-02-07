973101fa99828d1976e7d6ae6888c461f1ee20bd
TeraV <vharris1800@gmail.com>
2022-02-07 07:04:27 -0500

    Added 0.0.1.0 version information

--- src/main/java/net/javaserver/techniphilia/tools/ComponentToolsLoader.java
+++ src/main/java/net/javaserver/techniphilia/tools/ComponentToolsLoader.java
@@ -9,7 +9,7 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import org.apache.logging.log4j.Logger;
 import net.javaserver.techniphilia.core.ComponentCoreLoader;
-import net.javaserver.techniphilia.tools.common.item.PickaxeCopper;
+import net.javaserver.techniphilia.tools.common.item.*;
 import net.minecraft.item.Item;
 import net.minecraftforge.registries.IForgeRegistry;
 
@@ -37,10 +37,16 @@ public class ComponentToolsLoader {
     public static void registerItems(RegistryEvent.Register<Item> event) {
         IForgeRegistry<Item> registry = event.getRegistry();
         registry.register(PickaxeCopper.instance());
+        registry.register(PickaxeTin.instance());
+        registry.register(PickaxeBronze.instance());
+        registry.register(PickaxeAluminum.instance());
     }
     
     @SubscribeEvent
     public static void registerRenders(ModelRegistryEvent event) {
         ComponentCoreLoader.registerRender(PickaxeCopper.instance());
+        ComponentCoreLoader.registerRender(PickaxeTin.instance());
+        ComponentCoreLoader.registerRender(PickaxeBronze.instance());
+        ComponentCoreLoader.registerRender(PickaxeAluminum.instance());
     }
 }
--- src/main/java/net/javaserver/techniphilia/tools/common/ModRegistry.java
+++ src/main/java/net/javaserver/techniphilia/tools/common/ModRegistry.java
@@ -2,15 +2,14 @@ package net.javaserver.techniphilia.tools.common;
 
 import net.javaserver.techniphilia.tools.common.item.PickaxeCopper;
 import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import org.apache.logging.log4j.Logger;
 
 public class ModRegistry {
     public static final String MOD_NAME = "Techniphilia Tools";
     public static final String MOD_ID = "techniphilia-tools";
-    public static final String MOD_VERSION = "0.0.0.0";
-    public static final String MOD_DEPENDENCIES = "required-after:techniphilia-core@[1.0.1.0,2.0.0.0)";
+    public static final String MOD_VERSION = "0.0.1.0";
+    public static final String MOD_DEPENDENCIES = "required-after:techniphilia-core@[1.0.1.1,2.0.0.0)";
     public static Logger logger;
     private static ModItemGroup itemGroup = null;
     private ModRegistry() {}
